b20ad65ee8f36e0797d5408a5d49f0bc98d4f1bc
Andrey Savelyev <sandrig.dev@gmail.com>
2018-08-15 20:31:58 +0300

    feat: add capture in content

--- src/components/PhoneBody.vue
+++ src/components/PhoneBody.vue
@@ -19,12 +19,27 @@
         </filter-type>
       </div>
     </div>
+    <div v-if="step === 3">
+      <div class="selected-image"
+        :class="selectedFilter"
+        :style="{ backgroundImage: 'url(' + image + ')' }">
+      </div>
+      <div class="caption-container">
+        <textarea class="caption-input"
+          placeholder="Write a caption..."
+          type="text"
+          :value="value"
+          @input="$emit('input', $event.target.value)">
+        </textarea>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import VuegramPost from "./VuegramPost";
 import FilterType from "./FilterType";
+
 export default {
   name: "PhoneBody",
   props: {
@@ -32,7 +47,8 @@ export default {
     posts: Array,
     filters: Array,
     image: String,
-    selectedFilter: String
+    selectedFilter: String,
+    value: String
   },
   components: {
     "vuegram-post": VuegramPost,
